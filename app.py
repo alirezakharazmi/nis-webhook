29900fd4afb1509a6c44a0c0ac6924f4565f078a
alirezakharazmi <155370409+alirezakharazmi@users.noreply.github.com>
2025-04-17 11:33:18 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from attendance_bot import run_attendance
+
+app = Flask(__name__)
+
+@app.route('/run', methods=['GET'])
+def trigger():
+    run_attendance()
+    return 'Attendance triggered', 200
+
+if __name__ == '__main__':
+    app.run()

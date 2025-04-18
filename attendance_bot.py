789b55322ab24ef9f9eca074b50b00ab463e6c04
alirezakharazmi <155370409+alirezakharazmi@users.noreply.github.com>
2025-04-18 16:11:17 +0300

    Update attendance_bot.py

--- attendance_bot.py
+++ attendance_bot.py
@@ -7,12 +7,15 @@ def run_attendance_bot():
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
+
+    # این مسیر دقیق نصب شده‌ی Google Chrome است
     chrome_options.binary_location = "/opt/render/project/src/chrome/opt/google/chrome/google-chrome"
 
+    # این مسیر دقیق فایل کروم‌درایور unzip شده است
     service = Service("/opt/render/project/src/chrome/chromedriver-linux64/chromedriver")
 
     driver = webdriver.Chrome(service=service, options=chrome_options)
 
-    # باقی کدهای bot
+    # اجرای برنامه
     print("Attendance triggered")
     driver.quit()

2a55ba8c6577933314bf0dd9523d22ce211c146c
alirezakharazmi <155370409+alirezakharazmi@users.noreply.github.com>
2025-04-18 15:13:36 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# دانلود نسخه آماده‌ی گوگل کروم بدون نیاز به نصب
-mkdir -p /opt/chrome
+# دانلود و استخراج گوگل کروم در مسیر مجاز
+mkdir -p chrome
 curl -sSL https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -o chrome.deb
-dpkg-deb -xv chrome.deb /opt/chrome/
-export CHROME_BIN=/opt/chrome/opt/google/chrome/google-chrome
+dpkg-deb -xv chrome.deb chrome/
+export CHROME_BIN=$(pwd)/chrome/opt/google/chrome/google-chrome
 
 # اجرای برنامه
 gunicorn app:app --bind 0.0.0.0:$PORT

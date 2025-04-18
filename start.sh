dcbcc0d3582431c7204029de509c431b82f459bf
alirezakharazmi <155370409+alirezakharazmi@users.noreply.github.com>
2025-04-18 15:09:27 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-# نصب گوگل کروم
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt-get update
-apt-get install -y ./google-chrome-stable_current_amd64.deb
-
-# تنظیم مسیر کروم برای selenium
-export CHROME_BIN=/usr/bin/google-chrome
+# دانلود نسخه آماده‌ی گوگل کروم بدون نیاز به نصب
+mkdir -p /opt/chrome
+curl -sSL https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -o chrome.deb
+dpkg-deb -xv chrome.deb /opt/chrome/
+export CHROME_BIN=/opt/chrome/opt/google/chrome/google-chrome
 
 # اجرای برنامه
 gunicorn app:app --bind 0.0.0.0:$PORT

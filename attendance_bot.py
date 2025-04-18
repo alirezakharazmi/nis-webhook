8480b80a274f6b96982d3a3e8307790a997e74e8
alirezakharazmi <155370409+alirezakharazmi@users.noreply.github.com>
2025-04-18 15:41:16 +0300

    Update attendance_bot.py

--- attendance_bot.py
+++ attendance_bot.py
@@ -1,17 +1,18 @@
-from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-import os
 
 def run_attendance_bot():
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.binary_location = os.getenv("CHROME_BIN")
+    chrome_options.binary_location = "/opt/render/project/src/chrome/opt/google/chrome/google-chrome"
 
-    service = Service(executable_path="chromedriver")
+    service = Service("/opt/render/project/src/chrome/chromedriver-linux64/chromedriver")
 
     driver = webdriver.Chrome(service=service, options=chrome_options)
 
-    # ادامه فرم و اسکرین‌شات...
+    # باقی کدهای bot
+    print("Attendance triggered")
+    driver.quit()

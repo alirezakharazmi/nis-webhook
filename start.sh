b5c705c0a86e22adcd8b93e3e9948d4391cc38db
alirezakharazmi <155370409+alirezakharazmi@users.noreply.github.com>
2025-04-21 19:40:07 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,29 +1,23 @@
 #!/bin/bash
 
-# ساخت دایرکتوری برای chrome
 mkdir -p chrome
 
-# دانلود Google Chrome
+# دانلود و اکسترکت Google Chrome
 wget -q -O chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 ar x chrome.deb
 tar -xf data.tar.xz --directory=chrome ./opt/google/chrome/google-chrome
 
-# دانلود ChromeDriver (نسخه هماهنگ با کروم 122)
+# دانلود و اکسترکت ChromeDriver
 wget -q -O chromedriver.zip https://storage.googleapis.com/chrome-for-testing-public/122.0.6261.111/linux64/chromedriver-linux64.zip
 unzip -q chromedriver.zip -d chrome
 
-# دسترسی اجرایی به باینری‌ها
+# دسترسی اجرایی
 chmod +x chrome/opt/google/chrome/google-chrome
 chmod +x chrome/chromedriver-linux64/chromedriver
-
-# نمایش ساختار فایل‌ها (برای بررسی لاگ‌ها)
-echo "== Chrome & Driver Structure =="
-ls -l chrome/opt/google/chrome
-ls -l chrome/chromedriver-linux64
 
 # تنظیم متغیرهای محیطی
 export CHROME_BIN=$PWD/chrome/opt/google/chrome/google-chrome
 export PATH=$PWD/chrome/chromedriver-linux64:$PATH
 
-# اجرای اپلیکیشن
+# اجرای سرور
 gunicorn app:app --bind 0.0.0.0:$PORT

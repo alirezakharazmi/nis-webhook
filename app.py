5421cbf2d183c6ba5d27c92d73a29ad9cacdc66e
alirezakharazmi <155370409+alirezakharazmi@users.noreply.github.com>
2025-04-21 21:07:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "✅ Bot is running!"
+    return "Bot is running!"
 
 @app.route('/run')
 def run_bot():
     try:
         run_attendance_bot()
-        return "✅ Attendance triggered"
+        return "Attendance triggered"
     except Exception as e:
-        return f"❌ Error occurred: {str(e)}", 500
+        return f"Error: {str(e)}", 500

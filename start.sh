1f21f1932c1ea9aa903cff4f3e7fc4902dc147c1
alirezakharazmi <155370409+alirezakharazmi@users.noreply.github.com>
2025-04-21 21:28:23 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
+set -e
+
 mkdir -p chrome
 
+# نصب Chrome
 wget -q -O chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 ar x chrome.deb
-tar -xf data.tar.xz --directory=chrome ./opt/google/chrome
+tar -xf data.tar.xz --directory=chrome ./opt/google/chrome/google-chrome
 
+# دانلود ChromeDriver
 wget -q -O chromedriver.zip https://storage.googleapis.com/chrome-for-testing-public/122.0.6261.111/linux64/chromedriver-linux64.zip
 unzip -q chromedriver.zip -d chrome
 
+# سطح دسترسی اجرا
 chmod +x chrome/opt/google/chrome/google-chrome
 chmod +x chrome/chromedriver-linux64/chromedriver
 
+# تنظیم مسیرهای اجرایی
 export CHROME_BIN=$PWD/chrome/opt/google/chrome/google-chrome
 export PATH=$PWD/chrome/chromedriver-linux64:$PATH
 
+# اجرای اپلیکیشن
 gunicorn app:app --bind 0.0.0.0:$PORT
